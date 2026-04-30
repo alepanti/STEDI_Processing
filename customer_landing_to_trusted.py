ecf2b0e9e6684b5748e524f5523718448c1eb5f3
alepanti <alexispantig@gmail.com>
2026-04-30 14:57:29 +0000

    update to use visual etl

--- customer_landing_to_trusted.py
+++ customer_landing_to_trusted.py
@@ -7,7 +7,6 @@ from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from awsgluedq.transforms import EvaluateDataQuality
 from pyspark.context import SparkContext
-from pyspark.sql.functions import col, from_unixtime, to_timestamp
 
 
 def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
@@ -32,7 +31,7 @@ DEFAULT_DATA_QUALITY_RULESET = """
 """
 
 # Script generated for node customer_landing
-customer_landing_node1777404882965 = glueContext.create_dynamic_frame.from_options(
+customer_landing_node1777559555373 = glueContext.create_dynamic_frame.from_options(
     format_options={"multiLine": "false"},
     connection_type="s3",
     format="json",
@@ -40,43 +39,50 @@ customer_landing_node1777404882965 = glueContext.create_dynamic_frame.from_optio
         "paths": ["s3://d609-udacity/customer/landing/"],
         "recurse": True,
     },
-    transformation_ctx="customer_landing_node1777404882965",
+    transformation_ctx="customer_landing_node1777559555373",
 )
 
-# Script generated for node filter_shared
-SqlQuery155 = """
+# Script generated for node SQL Query
+SqlQuery0 = """
 select * from customer_landing
-where sharewithresearchasofdate is not null
+where sharewithresearchasofdate IS NOT NULL
 """
-filter_shared_node1777404988972 = sparkSqlQuery(
+SQLQuery_node1777559576428 = sparkSqlQuery(
     glueContext,
-    query=SqlQuery155,
-    mapping={"customer_landing": customer_landing_node1777404882965},
-    transformation_ctx="filter_shared_node1777404988972",
+    query=SqlQuery0,
+    mapping={"customer_landing": customer_landing_node1777559555373},
+    transformation_ctx="SQLQuery_node1777559576428",
 )
 
-# Convert date bigint to datetime
-df = filter_shared_node1777404988972.toDF()
-
-cols = [
-    "registrationdate",
-    "lastupdatedate",
-    "sharewithresearchasofdate",
-    "sharewithpublicasofdate",
-    "sharewithfriendsasofdate",
-]
-
-for c in cols:
-    df = df.withColumn(c, to_timestamp(from_unixtime(col(c) / 1000)))
-
-converted_time_df = DynamicFrame.fromDF(df, glueContext, "dynamic_frame")
+# Script generated for node change to timestamp
+changetotimestamp_node1777560024856 = ApplyMapping.apply(
+    frame=SQLQuery_node1777559576428,
+    mappings=[
+        ("customerName", "string", "customerName", "string"),
+        ("email", "string", "email", "string"),
+        ("phone", "string", "phone", "string"),
+        ("birthDay", "string", "birthDay", "date"),
+        ("serialNumber", "string", "serialNumber", "timestamp"),
+        ("registrationDate", "bigint", "registrationDate", "timestamp"),
+        ("lastUpdateDate", "bigint", "lastUpdateDate", "timestamp"),
+        (
+            "shareWithResearchAsOfDate",
+            "bigint",
+            "shareWithResearchAsOfDate",
+            "timestamp",
+        ),
+        ("shareWithPublicAsOfDate", "bigint", "shareWithPublicAsOfDate", "timestamp"),
+        ("shareWithFriendsAsOfDate", "bigint", "shareWithFriendsAsOfDate", "timestamp"),
+    ],
+    transformation_ctx="changetotimestamp_node1777560024856",
+)
 
 # Script generated for node customer_trusted
 EvaluateDataQuality().process_rows(
-    frame=converted_time_df,
+    frame=changetotimestamp_node1777560024856,
     ruleset=DEFAULT_DATA_QUALITY_RULESET,
     publishing_options={
-        "dataQualityEvaluationContext": "EvaluateDataQuality_node1777405602316",
+        "dataQualityEvaluationContext": "EvaluateDataQuality_node1777559511709",
         "enableDataQualityResultsPublishing": True,
     },
     additional_options={
@@ -84,17 +90,17 @@ EvaluateDataQuality().process_rows(
         "observations.scope": "ALL",
     },
 )
-customer_trusted_node1777405617789 = glueContext.getSink(
+customer_trusted_node1777559637708 = glueContext.getSink(
     path="s3://d609-udacity/customer/trusted/",
     connection_type="s3",
     updateBehavior="UPDATE_IN_DATABASE",
     partitionKeys=[],
     enableUpdateCatalog=True,
-    transformation_ctx="customer_trusted_node1777405617789",
+    transformation_ctx="customer_trusted_node1777559637708",
 )
-customer_trusted_node1777405617789.setCatalogInfo(
+customer_trusted_node1777559637708.setCatalogInfo(
     catalogDatabase="stedi_db", catalogTableName="customer_trusted"
 )
-customer_trusted_node1777405617789.setFormat("json")
-customer_trusted_node1777405617789.writeFrame(converted_time_df)
+customer_trusted_node1777559637708.setFormat("json")
+customer_trusted_node1777559637708.writeFrame(changetotimestamp_node1777560024856)
 job.commit()
